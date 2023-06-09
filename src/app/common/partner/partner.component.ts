bbb9e54b2fd7bb427cd05b5edcacca737f624e79
mithumills <106365983+mithumills@users.noreply.github.com>
2023-06-09 11:57:52 +1000

    feedback completed

--- src/app/common/partner/partner.component.ts
+++ src/app/common/partner/partner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +7,11 @@ import { Router } from '@angular/router';
     styleUrls: ['./partner.component.scss']
 })
 export class PartnerComponent implements OnInit {
-
+    backgroundStyle: any = {};
     constructor(
         public router: Router
     ) { }
 
-    ngOnInit(): void {}
+    ngOnInit(): void { }
 
 }
